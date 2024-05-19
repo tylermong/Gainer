487e02866e15eed230ada7644013e04e6646c782
tylermong <117101211+tylermong@users.noreply.github.com>
2024-05-19 17:19:59 -0400

    Changed sidebar options and added dropdown menu to workouts option

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,8 +1,16 @@
-import './index.css'; 
+import React, { useState } from 'react';
 import { IconBarbell } from '@tabler/icons-react';
+import { IconCaretRightFilled, IconCaretDownFilled } from '@tabler/icons-react';
+import './index.css'; 
 
 
 function Dashboard() {
+    const [isDropdownMenuOpen, setDropdownMenuOpen] = useState(false);
+
+    const toggleDropdownMenu = () => {
+        setDropdownMenuOpen(!isDropdownMenuOpen);
+    };
+
     return (
         <div id = "dashboard-container">
             <div className = "side-navbar">
@@ -11,8 +19,16 @@ function Dashboard() {
                     <h1 id = "name">Gainer</h1>
                 </div>
                 <div id = "nav-links">
-                    <a href = "#">Home</a>
-                    <a href = "#">Workouts</a>
+                    <a href = "#">Dashboard</a>
+                    <button onClick = {toggleDropdownMenu}>
+                        <div id = "workout-button">Workouts {isDropdownMenuOpen ? <IconCaretDownFilled size = {16} /> : <IconCaretRightFilled size = {16} />}</div>
+                    </button>
+                        {isDropdownMenuOpen && (
+                            <div id = "dropdown-menu-contents">
+                                <a href = "#">Create Workout</a>
+                                <a href = "#">View Workouts</a>
+                            </div>
+                        )}
                     <a href = "#">Nutrition</a>
                     <a href = "#">Settings</a>
                 </div>
