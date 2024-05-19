e75891b5c6b869a380d08f2e786e6c40bbb34d5d
Tyler Mong <117101211+tylermong@users.noreply.github.com>
2024-05-19 13:47:39 -0400

    Initial commit with test components

---
+++ gainer/src/App.tsx
@@ -0,0 +1,13 @@
+import Header from "./Header.tsx"
+import Footer from "./Footer.tsx"
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
+
+export default App
